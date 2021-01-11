346a7ac618c15928e66dd9a062ad17aca691e426
finesaaa <indriatifiqey16@gmail.com>
2021-01-11 15:33:06 +0700

    add card to laporan

--- resources/views/laporan/laba_rugi.blade.php
+++ resources/views/laporan/laba_rugi.blade.php
@@ -3,25 +3,30 @@
 @section('title', 'Keuangan Kapal | Laporan Laba Rugi')
 
 @section('content_header')
-<h1>LAPORAN LABA RUGI</h1>
+<h1>Laporan Laba Rugi</h1>
 @endsection
 
 @section('content')
-<div class="col">
-    <div class="col container">
-        <div class="row text-center">
+<div class="card">
+    <div class="card-header">
+        <div class="text-center pt-3">
             <div class="col">
                 <h5>PT. XYZ</h5>
                 <h6>Laporan Anggaran & Realisasi Laba Rugi</h6>
                 <h6>Pembangunan Rumah Pak David</h6>
-                <div class="row" style="padding-top: 4px; padding-bottom: 12px; padding-left: 10%; padding-right:10%">
-                    <div class="center-block" ><input class="date form-control text-center" type="text"></div>
-                    <div class="center-block sd" ><h6> s/d </h6></div>
-                    <div class="center-block"><input class="date form-control  text-center" type="text"></div>
-                </div>
-                
             </div>
         </div>
+        <div class="d-flex justify-content-center">
+            <div class="row" style="width: 400px; padding-top: 4px; padding-bottom: 12px;">
+                <div class="center-block"><input class="date form-control text-center" type="text"></div>
+                <div class="center-block"><h6> s/d </h6></div>
+                <div class="center-block"><input class="date form-control  text-center" type="text"></div>
+            </div>
+        </div>
+    </div>
+    <!-- /.card-header -->
+
+    <div class="card-body">
         <div class="row text-center pt-1">
             <div class="col">
                 <table id="table1"class="display table table-bordered table-hover dataTable">
@@ -39,7 +44,12 @@
             </div>
         </div>
     </div>
+    <!-- /.card-body -->
+
+    <div class="card-footer"></div>
+    <!-- /.card-footer -->
 </div>
+<!-- /.card -->
 @endsection
 
 @section('css')
@@ -54,6 +64,9 @@
     .table th + th
     {
         text-align:right
+    }
+    h5 {
+        font-weight: 600;
     }
     .col h6
     {
