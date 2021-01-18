9c28fec7f04995379c7136a6a850cd20128569d1
coll-j <zumazaki@gmail.com>
2021-01-19 02:05:50 +0700

    biji wijen profil perusahaan

--- resources/views/dashboard/profil_perusahaan.blade.php
+++ resources/views/dashboard/profil_perusahaan.blade.php
@@ -19,36 +19,38 @@
                         <li class="list-group-item">
                             <div class="row">
                                 <b class="col-md-3">Alamat</b> 
-                                <a class="col">Kampus ITS, Sukolilo, Jl. Raya ITS, Keputih, Surabaya, Jawa Timur 60117</a>
+                                <a id="alamat" class="col">Kampus ITS, Sukolilo, Jl. Raya ITS, Keputih, Surabaya, Jawa Timur 60117</a>
                             </div>
                         </li>
                         <li class="list-group-item">
                             <div class="row">
                                 <b class="col-md-3">Pemilik</b> 
-                                <a class="col">Dr. Angka</a>
+                                <a id="pemilik" class="col">Dr. Angka</a>
                             </div>
                         </li>
                         <li class="list-group-item">
                             <div class="row">
                                 <b class="col-md-3">Email</b> 
-                                <a class="col">pt-xyz@its.ac.id</a>
+                                <a id="email" class="col">pt-xyz@its.ac.id</a>
                             </div>
                         </li>
                         <li class="list-group-item">
                             <div class="row">
                                 <b class="col-md-3">Website</b> 
-                                <a class="col">its.ac.id</a>
+                                <a target="_blank" href="http://its.ac.id" id="web" class="col">its.ac.id</a>
                             </div>
                         </li>
                         <li class="list-group-item">
                             <div class="row">
-                                <b class="col-md-3">Telepon/Fax</b> 
-                                <a class="col">(0343) 123456</a>
+                                <b class="col-md-3">Telp/Fax</b> 
+                                <a id="telp" class="col">(0343) 123456</a>
                             </div>
                         </li>
                     </ul>
 
-                    <a href="#" class="btn btn-primary btn-block"><b>Edit</b></a>
+                    <a id="edit-perusahaan" href="#" class="btn btn-primary btn-block"><b>Edit</b></a>
+                    <a id="save-perusahaan" href="#" class="btn btn-primary btn-block" style="display: none;"><b>Save</b></a>
+
                 </div>
                 <!-- /.card-body -->
             </div>
@@ -61,8 +63,8 @@
                 <div class="card-header">
                     <h5>Anggota Perusahaan</h5>
                 </div>
-                <div class="card-header p-2">
-                    <ul class="nav nav-pills">
+                <div class="card-header bg-light">
+                    <ul class="nav nav-tabs card-header-tabs">
                         <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Lihat</a></li>
                         <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Tambah</a></li>
                     </ul>
@@ -70,8 +72,8 @@
                 <div class="card-body">
                 <div class="tab-content">
                     <div class="active tab-pane" id="activity">
-                        <button class="btn btn-sm btn-primary float-right ml-1"><i class="fas fa-pencil-alt"></i></button>
-                        <table id="table-member" class="display table table-stripped table-hover table-condensed table-sm dataTable">
+                        <!-- <button class="btn btn-sm btn-primary float-left ml-1"><i class="fas fa-pencil-alt"></i></button> -->
+                        <table id="table-member" class="display table table-hover table-condensed table-sm">
                             <thead class="thead-light">
                                 <th style="width: 60%">Nama</th>
                                 <th style="width: 40%">Jabatan</th>
@@ -176,13 +178,49 @@ h5 {
 @section('js')
 <script>
     $(document).ready(function() {
-        $('#table-member').DataTable({
+        $('table').SetEditable();
+        var table = $('#table-member').DataTable({
             paging      : false,
             searching   : true,
-            scrollY     : 320,
+            scrollY     : 250,
             scrollCollapse    : true,
             info        : false,
         });
+        
+        $('#edit-perusahaan').click(function(){
+            var temp_alamat = $('#alamat').html();
+            var temp_pemilik = $('#pemilik').html();
+            var temp_email = $('#email').html();
+            var temp_web = $('#web').html();
+            var temp_telp = $('#telp').html();
+
+            $(this).hide();
+            $('#save-perusahaan').show();
+            $('#alamat').html('<textarea rows="2" cols="30" style="resize: none;">' + temp_alamat + '</textarea>');
+            $('#pemilik').html('<input type="text" value="' + temp_pemilik + '">');
+            $('#email').html('<input type="text" value="' + temp_email + '">');
+
+            $('#web').removeAttr("target href");
+            $('#web').html('<input type="text" value="' + temp_web + '">');
+            $('#telp').html('<input type="text" value="' + temp_telp + '">');
+        });
+
+        $('#save-perusahaan').click(function(){
+            $(this).hide();
+            $('#edit-perusahaan').show();
+            // console.log();
+            $('#alamat').html($($('#alamat').children("textarea")[0]).html());
+            $('#pemilik').html( $($('#pemilik').children("input")[0]).val());
+            $('#email').html( $($('#email').children("input")[0]).val());
+            $('#web').attr({
+                href: "http://" + $($('#web').children("input")[0]).val(),
+                target: "_blank"});
+            $('#web').html( $($('#web').children("input")[0]).val());
+            $('#telp').html( $($('#telp').children("input")[0]).val());
+            
+        });
     } );
 </script>
+<script src="{{ asset('js/bootstable.js') }}"></script>
+
 @endsection
