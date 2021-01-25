f3a70a9dd82cc9ce77d8c930f8191749635c7525
finesaaa <indriatifiqey16@gmail.com>
2021-01-25 13:59:04 +0700

    edit invitation

--- resources/views/invitation.blade.php
+++ resources/views/invitation.blade.php
@@ -1,2 +1,11 @@
-<h1>Nyoba gan</h1>
-<a href="{{ route('reg', ['token' => $token]) }}">Test link</a>
+<h1>Registration Confirmation Email</h1>
+<p>
+Dear User,
+<br>
+<br>Welcome to the Keuangan Kapal Platform!
+<br>Please click on the link below to complete your registration.
+<br><a href="{{ route('reg', ['token' => $token]) }}">Registration Link</a>
+<br>Thank you for your support! :)
+</p>
+
+<p>If you have further question, do not hesitate to contact us in <a href="mailto:keuangan.kapal.2021@gmail.com">keuangan.kapal.2021@gmail.com</a></p>
